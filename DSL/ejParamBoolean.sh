02a95a078b3724b453d5b96991e81f6cce90bded
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-12-23 12:26:03 +0100

    1- configurar git scm

---
+++ DSL/ejParamBoolean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Bienvenido al ejercicio"
+if [ $agente == true ]
+then
+    echo "agente es true"
+else    
+    echo "agente es falso"
+fi
+echo "..."
+sleep 10
+echo "suerte ..."
