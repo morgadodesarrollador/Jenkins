29c8c26c810a4fcd326006f9283e132ea8a50ad3
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-01-31 11:52:47 +0100

    docker-compose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Bunenos dias clase ..., estamos con jenkins y NOS VAMOS A DESAyUNAR ...');
+  res.send('instalacion de la app via docker-compose ...');
 });
 
 var server = app.listen(3000, function () {
--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 #cd AppNodeJS
 #npm install
 echo "ejecución del contenedor"
-docker run -d --name appnode -p 3000:3000  morgadoberruezo/applibreria
+#docker run -d --name appnode -p 3000:3000  morgadoberruezo/applibreria
+docker-compose down
+docker-compose up 
