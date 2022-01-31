b7b46b851a599b3c6a63698ac562ac8324463987
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-01-31 11:55:00 +0100

    docker-compose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('instalacion de la app via docker-compose ...');
+  res.send('instalacion de la app via docker-compose ...., buenos dias clase ...');
 });
 
 var server = app.listen(3000, function () {
