2eb8e155ce3fdf0ac515096cd7026a7bcf6fb6cf
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-02-10 13:39:34 +0100

    server.js

--- pipelines/proyectos/appnodejs/index.js
+++ pipelines/proyectos/appnodejs/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Hola Mundo el ies cura valera!');
+  res.send('Hola Mundo, creación del server.js');
 });
 
 var server = app.listen(3000, function () {
